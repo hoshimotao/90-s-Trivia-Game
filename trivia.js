7543b12bc3eca53e909ead8708581e8888596077
Hoshimotao <jthomas302@gmail.com>
2019-09-04 18:21:11 -0400

    add new images, timer, and score

--- trivia.js
+++ trivia.js
@@ -1,76 +1,91 @@
 const questionsAndAnswers = [
   {
-    question: "Who is best known for their football-shaped head?",
-    correct: "Rocko",
-    incorrect: ["Arnold", "Dexter", "Goku"]  
+    question: "Who is best known for their football-shaped head?", 
+    image: "./images/arnold.jpg",
+    correct: "Arnold",
+    incorrect: ["Rocko", "Dexter", "Goku"]  
   },  
   {
     question: "Who considers themself a 'boy genius' and has their own lab?",
+    image: "./images/dexter.png",
     correct: "Dexter",
     incorrect: ["Sailor Moon", "Chucky", "Doug"]
   },
   {
     question: "Which of the following are made of sugar, spice, and everything nice?",
+    image: "./images/powerpuffgirls.jpg",
     correct: "Power Puff Girls",
     incorrect: ["Phil", "Jimmy", "Samurai Jack"]
   },
   {
     question: "Which of the following is the name of Scooby Doo's nephew?",
+    image: "./images/scrappy.jpg",
     correct: "Scrappy",
     incorrect: ["Joey", "Snappy", "Slappy"]
   },
   {
     question: "In the Rugrats, who is considered the 'scaredy cat' of the group?",
+    image: "./images/chuckie.jpg",
     correct: "Chuckie",
     incorrect: ["Chunky", "Lil", "Tommy"]
   },
   {
     question: "How many Pokemon were there in the first season of Pokemon?",
+    image: "./images/pokemon.jpg",
     correct: "151",
     incorrect: ["200", "149", "823"]
   },
   {
     question: "What was the name of Ash Ketchum's SECOND Pokemon?",
+    image: "./images/caterpie.jpg",
     correct: "Caterpie",
     incorrect: ["Charmander", "Pidgeot", "Mewtwo"]
   },
   {
     question: "In Dragon Ball Z, what is the name of Goku's wife?",
+    image: "./images/chichi.jpg",
     correct: "Chi-Chi",
     incorrect: ["Bulma", "Videl", "Mr. Popo"]
   },
   {
     question: "Who sacrificed their own life in an attempt to destroy Majin Buu in Dragon Ball Z?",
+    image: "./images/vegeta.gif",
     correct: "Vegeta",
     incorrect: ["Goku", "Gohan", "Krillin"]
   },
   {
     question: "In the show, Doug, who was Doug's best friend?",
+    image: "./images/skeeter.jpg",
     correct: "Skeeter",
     incorrect: ["Patty Mayonnaise", "Squirt", "Scooter"]
   },
   {
     question: "What color is the tip of Pikachu's tail?",
+    image: "./images/pikachuFace.png",
     correct: "Yellow",
     incorrect: ["Black", "Red", "Blue"]
   },
   {
     question: "In the show, Pokemon, why does Misty initially follow Ash?",
+    image: "./images/misty.jpg",
     correct: "To get her bike back",
     incorrect: ["Because she likes him", "He owes her money", "She wants to assassinate him"]
   },
   {
     question: "Complete the following title: 'Courage the cowardly ______?'",
+    image: "./images/courage.png",
     correct: "Dog",
     incorrect: ["Cat", "Bird", "Pup"]
   },
   {
     question: "In the show 'Hey, Arnold!,' who has a big crush on Arnold?",
+    image: "./images/heyArnold.jpg",
     correct: "Helga",
     incorrect: ["Olga", "Phoebe", "Harold"]
   },
   {
     question: "In the show Rocketpower, what is the term used to describe people who wear shoes at the beach?",
+    image: "./images/rocketPower.gif",
     correct: "Shoebies",
     incorrect: ["Tourists", "Shoobs", "Dorks"]
   }
--- triviaFunct.js
+++ triviaFunct.js
@@ -1,8 +1,54 @@
+// Timer 
+let timer = 35;
+let int = setInterval(function(){
+  console.log(timer)
+  timer--;
+  document.querySelector("#seconds").innerHTML = timer;
+  if(timer === 0){
+    alert(`Your Score: ${score.innerHTML}`)
+    clearInterval(int)
+  }
+}, 1000)
+//
+// This block is important and gives us a random question and img
 let randNum = Math.floor(Math.random() * questionsAndAnswers.length);
 
 let randomQ = questionsAndAnswers[randNum].question; 
 
 document.querySelector("#singleQ").innerHTML = randomQ;
+
+let randomImg = questionsAndAnswers[randNum].image;
+
+document.querySelector("#imgID").src = randomImg;
+
+// block ends here
+
+function newQ(){
+
+  randNum = Math.floor(Math.random() * questionsAndAnswers.length);
+  randomQ = questionsAndAnswers[randNum].question; 
+  document.querySelector("#singleQ").innerHTML = randomQ;
+  randomImg = questionsAndAnswers[randNum].image;
+  document.querySelector("#imgID").src = randomImg;
+
+  
+randomAns = questionsAndAnswers[randNum].incorrect;
+
+correctAns = questionsAndAnswers[randNum].correct;
+
+newArr = [randomAns[0], randomAns[1], randomAns[2], correctAns];
+
+newerArray = [];
+
+if(questionsAndAnswers.length > 0){
+
+newCopy = questionsAndAnswers.splice(randNum,1);
+
+}
+
+newAnswers();
+}
+
 
 let randomAns = questionsAndAnswers[randNum].incorrect;
 
@@ -10,32 +56,90 @@ let correctAns = questionsAndAnswers[randNum].correct;
 
 let newArr = [randomAns[0], randomAns[1], randomAns[2], correctAns];
 
-
-
 let newerArray = [];
 
+if(questionsAndAnswers.length > 0){
 
-while(newArr.length > 0){
+let newCopy = questionsAndAnswers.splice(randNum,1);
 
-  let rand = Math.floor(Math.random() * newArr.length);
-
-  console.log(newArr[rand]);
-
-   newerArray.push(newArr[rand])
-
-    newArr.splice(rand,1);
-  
-
+console.log(newCopy)
+console.log(questionsAndAnswers)
 }
 
-console.log(newerArray)
 
+
+function newAnswers(){
+while(newArr.length > 0){
+  let rand = Math.floor(Math.random() * newArr.length);
+  console.log(newArr[rand]);
+   newerArray.push(newArr[rand])
+    newArr.splice(rand,1);
+}
 document.querySelector("#one").innerHTML = newerArray[0];
 document.querySelector("#two").innerHTML = newerArray[1];
 document.querySelector("#three").innerHTML = newerArray[2];
 document.querySelector("#four").innerHTML = newerArray[3];
+}
+newQ()
 
-if(onClick.document.querySelector("#one").innerHTML == questionsAndAnswers.correct ){
 
-  alert("That is correct!")
+
+var score = document.querySelector("#score > h2 > span");
+var score2 = 0;
+
+console.log(score)
+console.log(score.innerHTML)
+
+// Button one
+function oneBtn(){
+  if(document.querySelector("#one").innerHTML == correctAns ){
+    alert("That is correct!");
+    score2 += 1;
+    score.innerHTML = score2;
+    newQ();     
+  }
+  else{
+    alert("EPIC FAIL!");
+    newQ();    
+  }  
+}
+// Button two
+function twoBtn(){
+  if(document.querySelector("#two").innerHTML == correctAns ){
+    alert("That is correct!");
+    score2 += 1;
+    score.innerHTML = score2;
+    newQ();    
+  }
+  else{
+    alert("EPIC FAIL!");
+    newQ();
+  }
+}
+// Button three
+function threeBtn(){
+  if(document.querySelector("#three").innerHTML == correctAns ){
+    alert("That is correct!");
+    score2 += 1;
+    score.innerHTML = score2;
+    newQ();
+  }
+  else{
+    alert("EPIC FAIL!");
+    newQ();
+  }
+}
+// Button four
+function fourBtn(){
+  if(document.querySelector("#four").innerHTML == correctAns ){
+    alert("That is correct!");
+    score2 += 1;
+    score.innerHTML = score2;
+    newQ();
+    
+  }
+  else{
+    alert("EPIC FAIL!");
+    newQ();
+  }
 }
